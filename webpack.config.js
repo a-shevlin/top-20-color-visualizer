48807ab73514931589d1a7295f78ce19251e1c3a
Seung Lee <shl7@uw.edu>
2022-07-05 14:26:38 -0700

    Add PKCE oauth

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const nodeExternals = require('webpack-node-externals');
-// npm install webpack-node-externals --save -dev
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
-  target: 'node',   // THIS IS THE IMPORTANT PART
-  externals: [nodeExternals()],
-  mode: 'development',
-//}
-  devtool: 'eval-source-map',  
-  devServer: {                 
-    contentBase: './dist'      
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist',
   },
   plugins: [
     new Dotenv(),
@@ -25,23 +19,20 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Spotify Color',
       template: './src/index.html',
-      inject: 'body'
-    })
+      inject: 'body',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader"
-      }
-    ]
-  }
+        loader: 'eslint-loader',
+      },
+    ],
+  },
 };
