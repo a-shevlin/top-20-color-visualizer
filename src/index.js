118b5886147bccc8ee81a3ccd42ac2e91b08b130
Alex Shevlin <alexshevlin1@gmail.com>
2022-07-05 11:01:16 -0700

    add spotify-web-api-node to app.js
    Co-authored-by: Seung Lee <leark@users.noreply.github.com>
    Co-authored-by: DonovanWeber <DonovanWeber@users.noreply.github.com>
    Co-authored-by: jpalaoro197 <jpalaoro197@users.noreply.github.com>
    Co-authored-by: Caroline Cerussi <carolinecerussi@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,4 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Template from './js/template.js';
+import SpotifyService from './js/app.js';
+
+// User Interface Logic
+
+$(function() {
+  // stuff here happens when document loads
+  getSpotifyStuff();
+
+
+});
+
+async function getSpotifyStuff() {
+  let spotifyApi = await SpotifyService.getSpotifyAPI();
+
+  spotifyApi.getAlbum('5U4W9E5WsYb2jUQWePT8Xm').then(
+    function(response) {
+      console.log(response.body);
+    },
+    function(error) {
+      console.log(error);
+    }
+  );
+}
